f95ad51b7d5bba12c1b84143c38d0fc10194e556
Alexei Sholik <alcosholik@gmail.com>
2012-09-08 14:38:25 +0300

    REfactor test.sh

--- test.sh
+++ test.sh
@@ -1,60 +1,26 @@
 #!/bin/sh
 
-./organize_changelog.py -o test/temp test/1 
-diff test/temp test/1.1
-if [ "$?" != "0" ]; then
-    echo 'Test 1 failed'
-    exit 1
-fi
+function test_script() {
+    test_name="$1"
+    test_result="$2"
+    shift 2
 
-./organize_changelog.py -o test/temp -n 1 test/1 
-diff test/temp test/1.2
-if [ "$?" != "0" ]; then
-    echo 'Test 2 failed'
-    exit 1
-fi
+    ./organize_changelog.py -o test/temp "$@"
+    diff test/temp "$test_result"
+    if [ "$?" != "0" ]; then
+        echo "$test_name failed"
+        exit 1
+    fi
+}
 
-./organize_changelog.py -o test/temp test/2
-diff test/temp test/2.1
-if [ "$?" != "0" ]; then
-    echo 'Test 3 failed'
-    exit 1
-fi
-
-./organize_changelog.py -o test/temp -n 1 test/2 
-diff test/temp test/2.2
-if [ "$?" != "0" ]; then
-    echo 'Test 4 failed'
-    exit 1
-fi
-
-./organize_changelog.py -o test/temp test/3
-diff test/temp test/3.1
-if [ "$?" != "0" ]; then
-    echo 'Test 5 failed'
-    exit 1
-fi
-
-./organize_changelog.py -o test/temp -n 2 test/3
-diff test/temp test/3.2
-if [ "$?" != "0" ]; then
-    echo 'Test 6 failed'
-    exit 1
-fi
-
-./organize_changelog.py -o test/temp -n -1 test/3
-diff test/temp test/3.2
-if [ "$?" != "0" ]; then
-    echo 'Test 7 failed'
-    exit 1
-fi
-
-./organize_changelog.py -o test/temp -n -1 test/1 
-diff test/temp test/1.1
-if [ "$?" != "0" ]; then
-    echo 'Test 8 failed'
-    exit 1
-fi
+test_script "Default test/1" test/1.1 test/1
+test_script "One section test/1" test/1.2 -n 1 test/1
+test_script "All sections test/1" test/1.1 -n -1 test/1
+test_script "Default test/2" test/2.1 test/2
+test_script "One section test/2" test/2.2 -n 1 test/2
+test_script "Default test/3" test/3.1 test/3
+test_script "Two sections test/3" test/3.2 -n 2 test/3
+test_script "All sections test/3" test/3.2 -n -1 test/3
 
 echo 'ok'
 rm test/temp
